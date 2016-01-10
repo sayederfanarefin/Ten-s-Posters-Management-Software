914b43ff33402ce83ab01cab67f7f31bf42049be
Sayed Erfan Arefin <erfan.arefin@hotmail.com>
2016-01-11 00:17:49 +0600

    yeah

---
+++ .net c# version/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.GData.Client;
+using Google.GData.Spreadsheets;
+
+
+namespace TENs_helping_tool
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
